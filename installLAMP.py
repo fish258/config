db0b3840c07c0450d8b17933c6feb813094ff95b
fish258 <44438335+fish258@users.noreply.github.com>
2019-08-20 16:34:58 +1200

    Update installLAMP.py

--- installLAMP.py
+++ installLAMP.py
@@ -2,7 +2,7 @@
 import os
 #vim has been installed automatically.
 os.system("sudo apt-get update")
-os.system("sh install.sh")
+os.system("sh /configSite/install.sh")
 os.system("sudo apt -y install apache2 php libapache2-mod-php")
 os.system("sudo apt -y install vim php-cli  php-intl php-xmlrpc php-soap php-mysql php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath")
 os.system("sudo service apache2 restart")

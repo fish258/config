89cc9fa5ae2eeda121049388b687517a0c513921
fish258 <44438335+fish258@users.noreply.github.com>
2019-09-26 15:37:10 +1200

    Create autoW.py

---
+++ autoW.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+file = open('/lib/systemd/system/rc.local.service','r') 
+lines = [] 
+for line in file:  
+    lines.append(line) 
+file.close() 
+lines.insert(14, '[Install]\nWantedBy=multi-user.target\nAlias=rc-local.service') 
+s = ''.join(lines) 
+file = open('/lib/systemd/system/rc.local.service', 'w+') 
+file.write(s) 
+file.close() 
+del lines[:] 
+os.system("sudo ln -s /lib/systemd/system/rc.local.service /etc/systemd/system/rc.local.service") 
+os.system("sudo touch /etc/rc.local") 
+os.system("sudo chmod +x /etc/rc.local") 
+fa = open('/etc/rc.local','w') 
+fa.write("#!/bin/bash\npython3 /home/ubuntu/myPython/changeIP.py") 
+fa.close()

157c404139b19b33cfdfd40873ba50d67c64d259
fish258 <44438335+fish258@users.noreply.github.com>
2019-09-26 16:35:48 +1200

    Update cIp.py

--- cIp.py
+++ cIp.py
@@ -7,13 +7,3 @@
  flist[20]="$CFG->wwwroot = 'http://%s/moodle';\n"%(publicIP) 
  f=open('/var/www/html/moodle/config.php','w+') 
  f.writelines(flist) 
-  
- #!/usr/bin/python 
- import os 
- p = os.popen("curl ifconfig.me") 
- publicIP = p.read() 
- f=open('/var/www/html/moodle/config.php','r+') 
- flist=f.readlines() 
- flist[20]="$CFG->wwwroot = 'http://%s/moodle';\n"%(publicIP) 
- f=open('/var/www/html/moodle/config.php','w+') 
- f.writelines(flist) 
